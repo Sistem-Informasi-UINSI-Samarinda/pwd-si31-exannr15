2beaba105ae38c7d13372685332e68007c3b785d
exannr15 <exannr15@gmail.com>
2025-11-12 15:16:14 +0800

    praktikum 1

---
+++ config/koneksi.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost";
+$database = "pwd_2441919058";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername,$username,$password,$database);
+
+if(!$conn){
+    die("koneksi gagal:".  mysqli_connect_error());
+
+}
+
+echo "koneksi berhasil";
+mysqli_close($conn);
+
+?>
